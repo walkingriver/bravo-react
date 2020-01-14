f079671de4443efe6c1e28bf5e3fefa0c72eb52c
Michael Callaghan <mikecallaghan67@gmail.com>
2020-01-14 06:20:52 -0500

    Working on instructions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,36 @@
 import {
   IonButtons,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
   IonContent,
   IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
   IonMenuButton,
   IonPage,
   IonTitle,
-  IonToolbar
-  } from '@ionic/react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
+  IonToolbar,
+  IonSlides,
+  IonSlide,
+  IonButton
+} from '@ionic/react';
 import React from 'react';
 import './Home.css';
 
 const HomePage: React.FC = () => {
+  let [showSkip] = React.useState(true);
+
+  const ionSlideChanged = (event: CustomEvent) => {
+    const target: any = event.target;
+    showSkip = !target.swiper.isEnd;
+    console.log('showSkip: ', showSkip);
+  }
+
+  const skipButton = () => {
+    console.log('Trying to show/hide the skip button');
+    if (showSkip) {
+      return (<IonButton routerDirection="forward" routerLink="/game" color="light">Skip</IonButton>);
+    } else {
+      return (<span></span>);
+    }
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -30,44 +39,22 @@ const HomePage: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>Home</IonTitle>
+          <IonButtons slot="end">
+            {skipButton()}
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonCard className="welcome-card">
-          <img src="/assets/shapes.svg" alt=""/>
-          <IonCardHeader>
-            <IonCardSubtitle>Get Started</IonCardSubtitle>
-            <IonCardTitle>Welcome to Ionic</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              Now that your app has been created, you'll want to start building out features and
-              components. Check out some of the resources below for next steps.
-            </p>
-          </IonCardContent>
-        </IonCard>
 
-        <IonList lines="none">
-          <IonListHeader>
-            <IonLabel>Resources</IonLabel>
-          </IonListHeader>
-          <IonItem href="https://ionicframework.com/docs/" target="_blank">
-            <IonIcon slot="start" color="medium" icon={book} />
-            <IonLabel>Ionic Documentation</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
-            <IonIcon slot="start" color="medium" icon={build} />
-            <IonLabel>Scaffold Out Your App</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
-            <IonIcon slot="start" color="medium" icon={grid} />
-            <IonLabel>Change Your App Layout</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
-            <IonIcon slot="start" color="medium" icon={colorFill} />
-            <IonLabel>Theme Your App</IonLabel>
-          </IonItem>
-        </IonList>
+        <IonSlides pager={true} onIonSlideDidChange={ionSlideChanged}>
+          <IonSlide>
+            01. Mike was here.
+    </IonSlide>
+          <IonSlide>
+            02. Mike was also here.
+    </IonSlide>
+        </IonSlides>
+
       </IonContent>
     </IonPage>
   );
